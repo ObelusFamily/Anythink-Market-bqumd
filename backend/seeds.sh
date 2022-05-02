9c0f1ee475882f9fc67283012c3ca373a1ff3c6a
dror rash <dror@transmitsecurity.com>
2022-05-02 08:32:13 +0300

    use mongosh and insertOne

--- backend/seeds.sh
+++ backend/seeds.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-
-mongo mongodb://127.0.0.1:27017/admin <<EOF
+mongosh mongodb://127.0.0.1:27017/admin <<EOF
 user = db.users.findOne()
 
 function randomString() {
   return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
 }
-db.items.insert({"title":"title","description":"description","image":"asdasd","tagList":[],"favorited":false,"favoritesCount":0,"seller":user._id,"slug":randomString()})
+db.items.insertOne({"title":"title 1","description":"description 1","image":"asdasd","tagList":[],"favorited":false,"favoritesCount":0,"seller":user._id,"slug":randomString()})
 EOF
