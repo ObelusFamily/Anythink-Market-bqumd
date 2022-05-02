65b7bc166973d57b4baaed4ad002fd954c27e512
dror rash <dror@transmitsecurity.com>
2022-05-02 08:16:11 +0300

    add dummy item to items collection

--- backend/seeds.sh
+++ backend/seeds.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-yarn seeds
+
+mongo mongodb://127.0.0.1:27017/admin <<EOF
+user = db.users.findOne()
+
+function randomString() {
+  return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
+}
+db.items.insert({"title":"title","description":"description","image":"asdasd","tagList":[],"favorited":false,"favoritesCount":0,"seller":user._id,"slug":randomString()})
+EOF
