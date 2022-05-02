eb5667bfab5039f930eadb501f170b25a4be55aa
dror rash <dror@transmitsecurity.com>
2022-05-02 10:17:00 +0300

    seed JS order process

--- backend/seeds.js
+++ backend/seeds.js
@@ -17,26 +17,29 @@ MongoClient.connect(url, async (err, db) => {
     if (err) throw err;
 
     console.log(`${res.insertedCount} users inserted; \n`, res);
+    
+    await dbo.collection("users").find({}).toArray(async (err, result) => {
+      if (err || !result) throw err;
+      console.log(`there are ${result.length} users in the database`);
+      const user = result[0];
+      
+      console.log('user is: ', JSON.stringify(user));
+      const items = [];
+      for (let i = 0; i < 100; i++) {
+        const item = {"title":"title" + randomString(),"description":"description" + randomString(),"image":`https://i.picsum.photos/id/383/200/301.jpg?hmac=TGwwM8PMWk_qJx0gBlfMpy-jSI-Vswo8thS1eH1CEwg`,"tagList":[],"favorited":false,"favoritesCount":0,"seller":user && user._id || randomString(),"slug":randomString()}
+        items.push(item);
+      }
+      await dbo.collection("items").insertMany(items, async (err, res) => {
+        if (err) throw err;
+        console.log(`${items.length} document inserted; \n`, res);
+        
+        console.log('Closing connectio; that was fun;');
+  
+  
+        db.close();
+      });
   })
 
-  await dbo.collection("users").find({}).toArray(async (err, result) => {
-    if (err || !result) throw err;
-    console.log(`there are ${result.length} users in the database`);
-    const user = result[0];
-    
-    console.log('user is: ', JSON.stringify(user));
-    const items = [];
-    for (let i = 0; i < 100; i++) {
-      const item = {"title":"title" + randomString(),"description":"description" + randomString(),"image":`https://i.picsum.photos/id/383/200/301.jpg?hmac=TGwwM8PMWk_qJx0gBlfMpy-jSI-Vswo8thS1eH1CEwg`,"tagList":[],"favorited":false,"favoritesCount":0,"seller":user && user._id || randomString(),"slug":randomString()}
-      items.push(item);
-    }
-    await dbo.collection("items").insertMany(items, async (err, res) => {
-      if (err) throw err;
-      console.log(`${items.length} document inserted; \n`, res);
-      
-      console.log('Closing connectio; that was fun;');
-      db.close();
-    });
     
   });
   
